44e203c1b27c9e5126ec0375af1deee76d8a1eca
leksebo <leksebo19@gmail.com>
2024-12-04 16:58:54 +0000

    Create player/enemy

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,21 +1,58 @@
-// DOM Elements
-const canvas = document.querySelector('canvas');
-const c = canvas.getContext('2d');
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
 
 canvas.width = 1024
 canvas.height = 576
 
 c.fillRect(0, 0, canvas.width, canvas.height)
-// Game State
+
+class Sprite {
+    constructor({ position, velocity }) {
+        this.position = position
+        this.velocity = velocity
+    }
+
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, 50, 150)
+    }
+}
 
 
 
-// Functions
+const player = new Sprite({
+    position: {
+    x: 0, 
+    y: 0
+    },
+    velocity: {
+    x: 0,
+    y: 0
+    }
+})
+
+player.draw()
+
+const enemy = new Sprite({
+    position: {
+    x: 400, 
+    y: 100
+    },
+    velocity: {
+    x: 0,
+    y: 0
+    }
+})
+
+enemy.draw()
+
+console.log(player);
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    console.log('go');
+}
+animate()
 
 
-
-
-  // Filter questions by difficulty
-
-// Event Listeners
 
