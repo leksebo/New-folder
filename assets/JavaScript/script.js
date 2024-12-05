bbf12753d4481d948413f958372995cc2d38dbf0
leksebo <leksebo19@gmail.com>
2024-12-05 01:33:41 +0000

    Move Characters with Event Listeners

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -6,7 +6,7 @@ canvas.height = 576
 
 c.fillRect(0, 0, canvas.width, canvas.height)
 
-const gravity = 0.2
+const gravity = 0.7
 class Sprite {
     constructor({ position, velocity }) {
         this.position = position
@@ -116,7 +116,7 @@ window.addEventListener('keydown', (event) => {
             player.lastKey = 'a'
             break
         case 'w':
-            player.velocity.y = -10
+            player.velocity.y = -20
             break
 
         case 'ArrowRight':
@@ -128,7 +128,7 @@ window.addEventListener('keydown', (event) => {
             enemy.lastKey = 'ArrowLeft'
             break
         case 'ArrowUp':
-            enemy.velocity.y = -10
+            enemy.velocity.y = -20
             break      
     }
     console.log(event.key);
