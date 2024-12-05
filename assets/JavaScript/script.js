24c61c4f8f54fa0158c7c66f3fef93b533e21133
leksebo <leksebo19@gmail.com>
2024-12-05 01:43:38 +0000

    added attack box

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -13,11 +13,25 @@ class Sprite {
         this.velocity = velocity
         this.height = 150
         this.lastKey
+        this.attackBox = {
+            position: this.position ,
+            width: 100,
+            height: 50,
+        }
     }
 
     draw() {
         c.fillStyle = 'red'
         c.fillRect(this.position.x, this.position.y, 50, this.height)
+
+        // attack box
+        c.fillStyle = 'green'
+        c.fillRect(
+            this.attackBox.position.x,
+            this.attackBox.position.y, 
+            this.attackBox.width, 
+            this.attackBox.height
+        )
     }
 
     update() {
