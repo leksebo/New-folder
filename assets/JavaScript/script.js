e38c767793aa5e635509c56a5b3dd3bc5dc53c08
leksebo <leksebo19@gmail.com>
2024-12-05 00:25:27 +0000

    create jump key

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -22,6 +22,7 @@ class Sprite {
     update() {
         this.draw()
         
+        this.position.x += this.velocity.x
         this.position.y += this.velocity.y
         
         if (this.position.y + this.height + this.velocity.y >= canvas.height) {
@@ -54,6 +55,20 @@ const enemy = new Sprite({
 
 console.log(player);
 
+
+const keys = {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+    w: {
+        pressed: false
+    }    
+}
+let lastKey
+
 function animate() {
     window.requestAnimationFrame(animate)
     console.log('go');
@@ -61,11 +76,48 @@ function animate() {
     c.fillRect(0, 0, canvas.width, canvas.height) 
     player.update()
     enemy.update()
+    
+    player.velocity.x = 0
+
+    if (keys.a.pressed && lastKey === 'a') {
+        player.velocity.x = -1
+    }   else if (keys.d.pressed && lastKey === 'd') {
+        player.velocity.x = 1
+    }
 }
 
 animate()
 
+window.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case 'd':
+            keys.d.pressed = true
+            lastKey = 'd'
+            break
+        case 'a':
+            keys.a.pressed = true
+            lastKey = 'a'
+            break
+        case 'w':
+            player.velocity.y = -10
+            lastKey = 'w'
+            break
+    }
+    console.log(event.key);
+})
 
-
-
+window.addEventListener('keyup', (event) => {
+    switch (event.key) {
+        case 'd':
+            keys.d.pressed = false
+            break
+        case 'a':
+            keys.a.pressed = false  
+            break
+        case 'w':
+            keys.w.pressed = false  
+            break
+    }
+    console.log(event.key);
+})
 
